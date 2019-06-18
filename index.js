945a11df0c5d103fc529838c32c892e2fac34748
Leonel Rojas <leonelrojasperez@gmail.com>
2019-06-17 23:27:54 -0400

    exponiendo imagenes

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require("express");
+const path = require('path')
+const express = require('express');
 const cors = require('cors')
 const app = express();
 const { PORT } = require("./env");
 
 // Configurar cabeceras y cors
 app.use(cors())
+
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 require("./routes/api")(app);
 require("./routes/views")(app);
